ac97d236cc55652f7b6456e14697d8f37833cf46
sofia <romata1966@gmail.com>
2021-05-12 21:57:33 +0200

    createEntities, cambiar ase al encontrar coma

--- TJE2021_3D/src/Stage.cpp
+++ TJE2021_3D/src/Stage.cpp
@@ -7,13 +7,16 @@
 void IntroStage::createEntities()
 {
 	Scene* scene = Game::instance->intro_scene;
-	string text = "data/RigtDoor.ASE data/LeftDoor.ASE data/ArcoDoor.ASE";
+	string text = "data/RigtDoor.ASE,data/LeftDoor.ASE,data/ArcoDoor.ASE";
 	string cad;
-
+	int found = -1;
+	int init = 0; 
 	for (int i = 0; i < MAX_ENT_INTRO; i++)
 	{
 		aux.push_back(new EntityMesh());
-		cad = text.substr(18 * i, 17);
+		init = found + 1;
+		found = text.find(",", found + 1);
+		cad = text.substr(init, found - init);
 		aux[i]->mesh = Mesh::Get(cad.c_str());
 		scene->entities.push_back(aux[i]);
 	}
